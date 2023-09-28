6cf3e1c34161737d51d3a9ae529fdb8ee406d33c
prajwal <[prjwlsingh@gmail.com]>
2023-09-29 01:04:59 +0530

    Revert "changes api to github api"
    This reverts commit 4d5dd76e5c60a9971e10e0eac5452ca8eef8ef87.

--- src/api.js
+++ src/api.js
@@ -1,40 +1,21 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
-const axios = require("axios");
 
 // Create an instance of the Express app
 const app = express();
 
 app.use(cors());
 
+
 // Create a router to handle routes
 const router = express.Router();
 
-// Define a route that makes a call to the GitHub API and responds with the top 15 users by followers
-router.get("/", async (req, res) => {
-  try {
-    const githubToken = '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'; // Get the GitHub token from environment variables
-    if (!githubToken) {
-      throw new Error("GitHub token not found in environment variables.");
-    }
-
-    const response = await axios.get("https://api.github.com/users", {
-      params: {
-        per_page: 15,
-        sort: "followers",
-      },
-      headers: {
-        Authorization: `Bearer ${githubToken}`,
-      },
-    });
-
-    const topUsers = response.data;
-    res.json(topUsers);
-  } catch (error) {
-    console.error("Error fetching GitHub data:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
 });
 
 // Use the router to handle requests to the `/.netlify/functions/api` path
